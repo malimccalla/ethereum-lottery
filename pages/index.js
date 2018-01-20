da8ed14c6e81164131e83cd6430e4a09d04484db
Mali Michael <malimccalla@gmail.com>
2018-01-20 18:01:47 +0000

    Set up web3

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
-const Index = () => <h1>Lottery</h1>;
+import React, { Component } from 'react';
+
+class Index extends Component {
+  state = { web3: null };
+
+  componentDidMount() {
+    const web3 = require('../lib/web3').default;
+    this.setState({ web3 });
+  }
+
+  render() {
+    const { web3 } = this.state;
+    if (!web3) return null;
+
+    return <h1>{web3.version}</h1>;
+  }
+}
 
 export default Index;
